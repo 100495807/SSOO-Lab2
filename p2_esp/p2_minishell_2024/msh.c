a396bf6b54dea6f2dc35c670f3a522a35cc81111
100495807 <158489101+100495807@users.noreply.github.com>
2024-04-09 11:49:25 +0200

    Update msh.c

--- p2_esp/p2_minishell_2024/msh.c
+++ p2_esp/p2_minishell_2024/msh.c
@@ -33,7 +33,65 @@ void siginthandler(int param)
 }
 
 /* myhistory */
+void mycalc(char ***argvv){
 
+    // TODO: fix accum
+
+	/* Check if the command is mycalc */
+	if (strcmp(argvv[0][0], "mycalc") == 0){
+
+		/* Check if the input command is composed by operand1 add/mod and operand 2 */
+		if (argvv[0][1] != NULL && argvv[0][2] != NULL && argvv[0][3] != NULL){
+			
+			/* Get the operands */
+			int op1 = atoi(argvv[0][1]);
+			int op2 = atoi(argvv[0][3]);
+			
+			/* If add define the accumulator and show the result in the standard error output */
+			if (strcmp(argvv[0][2],"add")==0){
+				int accum = accum + op1 + op2 ;
+                int add = op1 + op2;
+				char buf_add[50];
+				sprintf(buf_add, "[OK] %d + %d = %d Acc %d\n", op1, op2, add, accum);
+				
+				/* Write in standard output error and if there is an error show the error */
+				if (write(2, buf_add, strlen(buf_add)) < strlen(buf_add)){
+					perror("Error in write\n");
+				}
+			}
+			
+			/* If mod calculate the remainider the quotient and show the result in the standard error output */
+			else if (strcmp(argvv[0][2],"mod")==0){
+				int rem = op1 % op2;
+				int quo = op1 / op2;
+				char buf_mod[50];
+				sprintf(buf_mod, "[OK] %d %% %d = %d * %d + %d\n", op1, op2, op2, quo, rem);
+				
+				/* Write in standard output error and if there is an error show the error */
+				if (write(2, buf_mod, strlen(buf_mod)) < strlen(buf_mod)){
+					perror("Error in write\n");
+				}
+			}
+			
+			/* If the input does not follow the the structure show the error in the standard output */
+			else{
+				/* Write in standard output and if there is an error show the error */
+				if (write(1,"[ERROR] The structure of the command is <operand 1> <add/mod> <operand 2>\n", strlen("[ERROR] The structure of the command is <operand 1> <add/mod> <operand 2>\n")) < strlen("[ERROR] The structure of the command is <operand 1> <add/mod> <operand 2>\n")){
+					perror("Error in write\n");
+				}
+			}
+		} 
+		
+		/* If the input does not follow the the structure show the error in the standard output */
+		else{
+			/* Write in standard output and if there is an error show the error */
+			if (write(1,"[ERROR] The structure of the command is <operand 1> <add/mod> <operand 2>\n", strlen("[ERROR] The structure of the command is <operand 1> <add/mod> <operand 2>\n")) < strlen("[ERROR] The structure of the command is <operand 1> <add/mod> <operand 2>\n")){
+				perror("Error in write\n");
+			}
+		}
+	}
+    return;           
+}
 /* myhistory */
 
 struct command
@@ -192,7 +250,7 @@ int main(int argc, char* argv[])
             command_counter = read_command(&argvv, filev, &in_background); //NORMAL MODE
     }
 		//************************************************************************************************
-        
+        int curr_command = -1; // identifies which child is in charge of which command
 
 		/************************ STUDENTS CODE ********************************/
 	   if (command_counter > 0) {
@@ -200,50 +258,114 @@ int main(int argc, char* argv[])
 				printf("Error: Maximum number of commands is %d \n", MAX_COMMANDS);
 			}
 			else {
-				// Print command
-				//print_command(argvv, filev, in_background);
-                /*int status;
-                int pid = fork();
-                if (pid==0){
-                    execvp(argvv[0][0], argvv[0]);}
-                else{
-                    wait(&status);
-                
-                }*/
-                int status;
-                int p1[2];
-                pipe(p1);
-                int pid;
-                pid = fork();
-                switch (pid)
-                {
-                case -1:
-                    printf("Error");
-                    break;
-                case 0:
-                    int pid2;
-                    pid2 = fork();
-                    switch (pid2)
-                    {
-                    case 0:
-                        close(1);
-                        dup(p1[1]);
-                        close(p1[1]);
-                        close(p1[0]);
-                        execvp(argvv[1][0], argvv[1]);
+				/* PIPES */
+                /* setup pipes */
+                int pipe0[2], pipe1[2];
+                pipe(pipe0);
+                pipe(pipe1);
+
+                /* create subprocesses */
+
+                int pid = 1;
+                for (int i = 0; i < command_counter; i++){
+                    if (pid != 0){ // only father can fork
+                        if ((pid = fork()) == 0){ /* child */
+                            curr_command = i;
+                        }
+                    } else break;
+                }
+
+                if (pid == -1){ /* error */
+                    perror("Error in fork");
+                    return -1;
+                } else if (pid != 0){ /* parent process */
+
+                    /* close pipes */
+                    close(pipe0[0,1]);
+                    close(pipe1[0,1]);
+
+                    /* BACKGROUND */
+                           
+                    if (in_background != 1){
+                        while (wait(&status) != pid){ // wait for children to finish
+                            if (status != 0){
+                                perror("Error executing the child");
+                            } 
+                        }
+                    }
+                } else { /* child process */
+                           
+                    /* REDIRECTION */
+                    
+                    if ((curr_command == 0) && (filev[0][0] != '0')){
+                        /* redirect from input, file[0] as stdin */
+                        close(STDIN_FILENO); // free file desc. 0
+                        int fd = open(filev[0], O_RDONLY); // fd is now 0
+                    } else if ((curr_command == command_counter - 1) && (filev[1][0] != '0')){
+                        /* redirect to output, file[1] as stdout */
+                        close(STDOUT_FILENO);
+                        int fd = open(filev[1], O_CREAT | O_RDWR, S_IRWXU);                          
+                    }
+                    if (filev[2][0] != '0'){
+                        /* redirect error, file[1] as stderr */
+                        close(STDERR_FILENO);
+                        int fd = open(filev[2], O_CREAT | O_RDWR, S_IRWXU);                          
+                    }
+
+                    /* PIPES */
+
+                    if (command_counter > 1){
+                        if (curr_command == 0){ /* first command - pipe0 out */
+                            close(pipe1[0,1]);
+                            close(pipe0[0]);
+                            dup2(pipe0[1], STDOUT_FILENO); // stdout is now pipe write
+                            close(pipe0[1]);
+                        } else if (curr_command == command_counter - 1){ /* last command - pipe in */
+                            if ((curr_command % 2) != 0){ // odd - reads from pipe0
+                                close(pipe1[0,1]);
+                                close(pipe0[1]);
+                                dup2(pipe0[0], STDIN_FILENO); // stdin is now pipe read
+                                close(pipe0[0]);
+                            } else{ /* even - pipe1 in */
+                                close(pipe0[0,1]);
+                                close(pipe1[1]);
+                                dup2(pipe1[0], STDIN_FILENO);
+                                close(pipe1[0]);
+                            }
+                        } else if ((curr_command % 2) != 0){ /* intermediate command, odd - pipe in/out */
+                            close(pipe0[1]);
+                            close(pipe1[0]);
+                            dup2(pipe0[0], STDIN_FILENO);
+                            dup2(pipe1[1], STDOUT_FILENO);
+                        } else{ /* intermediate command, even - pipe in/out */
+                            close(pipe1[1]);
+                            close(pipe0[0]);
+                            dup2(pipe1[0], STDIN_FILENO); // as it's even, we have to alternate to complete the loop
+                            close(pipe1[0]);
+                            dup2(pipe0[1], STDOUT_FILENO);
+                            close(pipe0[1]);
+                        }
+                        /* close pipes *
+                        close(pipe0[0,1]);
+                        close(pipe1[0,1]);*/
+                    }
+                    /* execute current command */
+
+                    /* INTERNAL COMMANDS */
+                         
+                    if (strcmp(argvv[curr_command][0], "mycalc") == 0){
+                        /* execute mycalc */
+                        mycalc(argvv);
                         exit(0);
-                    default:
-                        
-                        close(0);
-                        dup(p1[0]);
-                        close(p1[0]);
-                        close(p1[1]);
-                        execvp(argvv[0][0], argvv[0]);
-                        exit(1);
+                    } else{
+                        /* COMMAND EXECUTION */
+
+                        getCompleteCommand(argvv, curr_command);
+                        execvp(argvv[curr_command][0], argvv[curr_command]); //execute the comand
+                        perror("Error in execvp\n");
+                        exit(0);
                     }
-                default:
-                    wait(&status);
-                    }
+                }
 			}
 		}
 	}
